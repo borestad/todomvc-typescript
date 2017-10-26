94194ce1142311e7dd05b34eec91e56c02b1b00c
Johan Borestad <johan@borestad.com>
2017-10-26 10:24:16 +0200

    bump

--- src/app.tsx
+++ src/app.tsx
@@ -1,30 +1,50 @@
+// tslint:disable:jsx-no-lambda
 import { classNames, h, render } from './dom'
 const $app = document.getElementById('app')
 
-const initialState = [{
-  text: 'Vanilladux',
-  completed: false,
-  id: 0
-}]
+let initialState = [
+  {
+    text: 'Vanilladux',
+    completed: false,
+    id: 0
+  }
+]
+
+try {
+  const store = localStorage.getItem('store')
+  if (store) {
+    initialState = JSON.parse(store) as any
+  }
+} catch (e) {
+  console.error(e)
+}
+// if (localStorage.store) {
+//   initialState = JSON.parse(localStorage.getItem('store')) as any
+// }
 
 let store = [...initialState]
 
 // TODO: use store.dispatch
 // http://redux.js.org/docs/api/Store.html#dispatch
-const dispatch = (fn) =>
-  document.dispatchEvent(new CustomEvent('action', {
-    detail: fn
-  }))
+const dispatch = fn =>
+  document.dispatchEvent(
+    new CustomEvent('action', {
+      detail: fn
+    })
+  )
 
-const reducers = (state, action) =>
-  todoReducer(state, action)
+const reducers = (state, action) => todoReducer(state, action)
 
-document.addEventListener('action', function (e) {
-  console.log('action happened', e)
-  store = reducers(store, (e as any).detail)
-  console.log(store)
-  document.dispatchEvent(new CustomEvent('state'))
-}, false)
+document.addEventListener(
+  'action',
+  function (e) {
+    store = reducers(store, (e as any).detail)
+    console.log('Action:', (e as any).detail, store)
+    localStorage.store = JSON.stringify(store)
+    document.dispatchEvent(new CustomEvent('state'))
+  },
+  false
+)
 
 enum Keys {
   ENTER = 13,
@@ -46,23 +66,6 @@ const store2 = {
 
 // Controller
 // ==============================================================
-const onclick = () => {
-  alert('Hellooooo')
-}
-
-function addTodo (e: KeyboardEvent) {
-  if (e.keyCode === Keys.ENTER) {
-    const text = (e.target as HTMLInputElement).value.trim()
-    dispatch(Actions.addTodo(text))
-  }
-}
-
-const update = () => render($app, View({ todos: store }))
-document.addEventListener('state', (e) => update())
-const i = 0
-document.getElementById('reload').addEventListener('click', update)
-
-requestAnimationFrame(update)
 
 // ========================================================
 // Actions
@@ -75,6 +78,34 @@ export const Actions = {
   completeAll: () => ({ type: 'COMPLETE_ALL' }),
   clearCompleted: () => ({ type: 'CLEAR_COMPLETED' })
 }
+
+const Controller = {
+  addTodo: (e: KeyboardEvent) => {
+    if (e.keyCode === Keys.ENTER) {
+      const text = (e.target as HTMLInputElement).value.trim()
+      text.length && dispatch({ type: 'ADD_TODO', text })
+    }
+  },
+  completeTodo: id => {
+    dispatch(({ type: 'COMPLETE_TODO', id }))
+  },
+  deleteTodo: id => {
+    dispatch({ type: 'DELETE_TODO', id })
+  },
+  completeAll: () => {
+    dispatch({ type: 'COMPLETE_ALL' })
+  },
+  clearCompleted: () => {
+    dispatch({ type: 'CLEAR_COMPLETED' })
+  }
+}
+
+const update = () => render($app, View({ todos: store, c: Controller }))
+document.addEventListener('state', e => update())
+const i = 0
+document.getElementById('reload').addEventListener('click', update)
+
+requestAnimationFrame(update)
 
 // ========================================================
 // Reducer
@@ -91,22 +122,17 @@ function todoReducer (state = initialState, action) {
       ]
 
     case 'DELETE_TODO':
-      return state.filter(todo =>
-        todo.id !== action.id
-      )
+      return state.filter(todo => todo.id !== action.id)
 
     case 'EDIT_TODO':
-      return state.map(todo =>
-        todo.id === action.id ?
-          { ...todo, text: action.text } :
-          todo
+      return state.map(
+        todo => (todo.id === action.id ? { ...todo, text: action.text } : todo)
       )
 
     case 'COMPLETE_TODO':
-      return state.map(todo =>
-        todo.id === action.id ?
-          { ...todo, completed: !todo.completed } :
-          todo
+      return state.map(
+        todo =>
+          todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
       )
 
     case 'COMPLETE_ALL':
@@ -126,15 +152,18 @@ function todoReducer (state = initialState, action) {
 // ========================================================
 // VIEW
 // ========================================================
-const itemsLeft = todos =>
-  todos.reduce((sum, { completed }) => (completed ? sum : sum + 1), 0)
 
-const View = ({ todos }) => (
+const View = ({ todos, c }) => (
   <div class='app-container'>
     <section class='todoapp'>
       <header class='header'>
         <h1>todos</h1>
-        <input onkeypress={addTodo} class='new-todo' autofocus={true} placeholder='What needs to be done?' />
+        <input
+          onkeypress={c.addTodo}
+          class='new-todo'
+          autofocus={true}
+          placeholder='What needs to be done?'
+        />
       </header>
 
       <section class='main'>
@@ -144,7 +173,7 @@ const View = ({ todos }) => (
       </section>
 
       <footer class='footer'>
-        <TodoCounter todos={todos}/>
+        <TodoCounter todos={todos} />
         {/* <!-- Remove this if you don't implement routing --> */}
         <ul class='filters'>
           <li><a href='#/' class='selected'>All</a></li>
@@ -152,35 +181,36 @@ const View = ({ todos }) => (
           <li><a href='#/completed'>Completed</a></li>
         </ul>
         {/* <!-- Hidden if no completed items are left ↓ --> */}
-        {false && <button class='clear-completed'>Clear completed</button>}
+        {true && <button class='clear-completed'>Clear completed</button>}
       </footer>
     </section>
 
-    <FooterInfo />
+    <footer class='info'>
+      <p>Double-click to edit a todo</p>
+      <p> Created by <a href='https://github.com/borestad'>Johan Borestad</a></p>
+      <p>Part of <a href='http://todomvc.com'>TodoMVC</a></p>
+    </footer>
   </div>
 )
 
-const TodoItem = ({ editing, completed, text, value }) => (
-  <li class={classNames({ completed, editing })}>
+const TodoItem = ({ id, editing, completed, text, value }) => (
+  <li key={id} class={classNames({ completed, editing })}>
     <div class='view'>
-      <input class='toggle' type='checkbox' checked />
+      <input
+        class='toggle' type='checkbox' checked={completed} onChange={() => Controller.completeTodo(id)} />
       <label>{text}</label>
-      <button class='destroy' />
+      <button class='destroy' onClick={Controller.deleteTodo.bind(null, id)} />
     </div>
     <input class='edit' value={value} />
   </li>
+)
+
+const itemsLeft = todos => (
+  todos.reduce((sum, { completed }) => (completed ? sum : sum + 1), 0)
 )
 
 const TodoCounter = ({ todos }) => (
   <span class='todo-count'>
     <strong>{itemsLeft(todos)}</strong> item left
   </span>
-
-)
-const FooterInfo = () => (
-  <footer class='info'>
-  <p>Double-click to edit a todo</p>
-  <p>Created by <a href='https://github.com/borestad'>Johan Borestad</a></p>
-  <p>Part of <a href='http://todomvc.com'>TodoMVC</a></p>
-</footer>
 )
